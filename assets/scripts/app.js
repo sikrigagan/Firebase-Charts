76625cbc2ed0ca9771514183ab2eee48c7f52081
Gagan Sikri <sikrigagan@gmail.com>
2017-11-15 16:22:17 +0530

    Added Code

---
+++ assets/scripts/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCUGTrY5OD1uBupNIHvCRxEO1NKYVlPtDM",
+    authDomain: "fusioncharts-demo.firebaseapp.com",
+    databaseURL: "https://fusioncharts-demo.firebaseio.com",
+    projectId: "fusioncharts-demo",
+    storageBucket: "fusioncharts-demo.appspot.com",
+    messagingSenderId: "728123238984"
+};
+firebase.initializeApp(config);
